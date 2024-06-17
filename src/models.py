eb9df08c7ea6e7aa82175eabdbfb99c541159a13
Muhammad Amir <92692137+Mamir21@users.noreply.github.com>
2024-06-17 12:03:26 -0400

    Create models.py

---
+++ src/models.py
@@ -0,0 +1,22 @@
+# models.py
+from db.py import get_db_connection
+
+def add_transaction(date, category, amount, type):
+    with get_db_connection() as conn:
+        conn.execute('''INSERT INTO transactions (date, category, amount, type) 
+                        VALUES (?, ?, ?, ?)''', (date, category, amount, type))
+
+def get_transactions():
+    with get_db_connection() as conn:
+        transactions = conn.execute('SELECT * FROM transactions').fetchall()
+        return transactions
+
+def get_transactions_by_type(type):
+    with get_db_connection() as conn:
+        transactions = conn.execute('SELECT * FROM transactions WHERE type = ?', (type,)).fetchall()
+        return transactions
+
+def get_transactions_by_category(category):
+    with get_db_connection() as conn:
+        transactions = conn.execute('SELECT * FROM transactions WHERE category = ?', (category,)).fetchall()
+        return transactions
